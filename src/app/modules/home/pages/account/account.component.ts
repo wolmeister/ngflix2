d0d13405b8c22c5f87795fc502edb512c04b21c8
Victor Wolmeister <wolmeistervictor@gmail.com>
2020-06-22 00:48:34 -0300

    Implement account page

--- src/app/modules/home/pages/account/account.component.ts
+++ src/app/modules/home/pages/account/account.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+
+import { AppState } from '../../../../store/app-store.state';
+import { User } from '../../../../models/user';
 
 @Component({
   selector: 'app-account',
   templateUrl: './account.component.html',
-  styleUrls: ['./account.component.scss']
+  styleUrls: ['./account.component.scss'],
 })
 export class AccountComponent implements OnInit {
+  user: User;
 
-  constructor() { }
+  constructor(private store: Store<AppState>) {}
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.store
+      .select((state) => state.auth.user)
+      .subscribe((user) => {
+        this.user = user;
+      });
   }
-
 }
