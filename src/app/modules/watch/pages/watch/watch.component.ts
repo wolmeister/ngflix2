238c9235a16105b13b8cd62c0fb7c9a6ae0d85ee
Victor Wolmeister <wolmeistervictor@gmail.com>
2020-06-22 02:37:56 -0300

    Implement watch page

--- src/app/modules/watch/pages/watch/watch.component.ts
+++ src/app/modules/watch/pages/watch/watch.component.ts
@@ -1,15 +1,119 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { ActivatedRoute, Router } from '@angular/router';
+
+import { AppState } from '../../../../store/app-store.state';
+import { MediaProgressService } from '../../../../services/media-progress.service';
+import { MediaService } from '../../../../services/media.service';
+import { Media } from '../../../../models/media';
+import { User } from '../../../../models/user';
+import { getUrl } from '../../../../utils/route.utils';
+import { RoutesConfig } from '../../../../configs/routes.config';
 
 @Component({
   selector: 'app-watch',
   templateUrl: './watch.component.html',
-  styleUrls: ['./watch.component.scss']
+  styleUrls: ['./watch.component.scss'],
 })
 export class WatchComponent implements OnInit {
+  plyrOptions = {
+    controls: [
+      'play-large',
+      'play',
+      'rewind',
+      'fast-forward',
+      'volume',
+      'progress',
+      'current-time',
+      'fullscreen',
+    ],
+  };
+  plyrSources = [];
+  player: Plyr;
+  user: User;
+  media: Media;
+  showBackIcon = false;
+  loaded = false;
 
-  constructor() { }
+  constructor(
+    private store: Store<AppState>,
+    private route: ActivatedRoute,
+    private router: Router,
+    private mediaService: MediaService,
+    private mediaProgressService: MediaProgressService
+  ) {}
 
   ngOnInit(): void {
+    this.store
+      .select((state) => state.auth.user)
+      .subscribe((user) => {
+        this.user = user;
+      });
+    this.route.paramMap.subscribe((paramMap) => {
+      const mediaId = parseInt(paramMap.get('id'), 10);
+      const media = this.mediaService.getMedia(mediaId);
+      if (!media) {
+        const url = getUrl(RoutesConfig.home.home, RoutesConfig.home.basePath);
+        this.router.navigateByUrl(url);
+        return;
+      }
+      this.media = media;
+      this.plyrSources = [
+        {
+          src: media.file,
+          type: 'video/mp4',
+        },
+      ];
+    });
   }
 
+  loadedData() {
+    const mp = this.mediaProgressService.getMediaProgress(
+      this.user,
+      this.media
+    );
+    this.player.currentTime = mp && !mp.finished ? mp.progress : 0;
+    this.loaded = true;
+    this.player.play();
+  }
+
+  controlsShown() {
+    this.showBackIcon = true;
+  }
+
+  controlsHidden() {
+    this.showBackIcon = false;
+  }
+
+  timeUpdate() {
+    if (!this.loaded) {
+      return;
+    }
+    const { currentTime } = this.player;
+    let mp = this.mediaProgressService.getMediaProgress(this.user, this.media);
+    if (mp) {
+      mp.finished = false;
+      mp.progress = currentTime;
+    } else {
+      mp = {
+        userId: this.user.id,
+        mediaId: this.media.id,
+        finished: false,
+        progress: currentTime,
+      };
+    }
+    this.mediaProgressService.saveMediaProgress(mp);
+  }
+
+  ended() {
+    const mp = this.mediaProgressService.getMediaProgress(
+      this.user,
+      this.media
+    );
+    // will never be null
+    if (mp) {
+      mp.finished = true;
+      this.mediaProgressService.saveMediaProgress(mp);
+    }
+  }
 }
--- src/app/modules/watch/watch.module.ts
+++ src/app/modules/watch/watch.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { PlyrModule } from 'ngx-plyr';
+import { FeatherModule } from 'angular-feather';
+import { ArrowLeft } from 'angular-feather/icons';
+
 import { WatchRoutingModule } from './watch-routing.module';
 import { WatchComponent } from './pages/watch/watch.component';
 
 @NgModule({
   declarations: [WatchComponent],
-  imports: [CommonModule, WatchRoutingModule],
+  imports: [
+    CommonModule,
+    WatchRoutingModule,
+    PlyrModule,
+    FeatherModule.pick({ ArrowLeft }),
+  ],
 })
 export class WatchModule {}
